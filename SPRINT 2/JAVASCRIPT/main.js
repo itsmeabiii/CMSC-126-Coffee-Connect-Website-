7cb7d2a420e950783bf067bc10069ffb19bf7eb5
Abigail Davocol <57359818+itsmeabiii@users.noreply.github.com>
2021-06-26 22:17:18 +0800

    Add files via upload

---
+++ SPRINT 2/JAVASCRIPT/main.js
@@ -0,0 +1,8 @@
+
+document.getElementById('button1').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close_but').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
